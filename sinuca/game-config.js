e7a1ca8c43b4c7c508cec10070a99ef9f70f8d31
Pegasus77-Adriel <61716547+Pegasus77-Adriel@users.noreply.github.com>
2023-04-12 21:27:39 -0300

    Add files via upload

---
+++ sinuca/game-config.js
@@ -0,0 +1,177 @@
+CANVAS_W = 480;
+CANVAS_H = 640;
+
+
+
+entities["TableImg"] = {active:true, hidden:false,
+	position:{x:CANVAS_W/2,y:CANVAS_H/2}, w:1277, h:704, rotation:0, flip:"none",
+	frames:[{index:"frame1",img:"../images/Table.png"}],
+	framesToShow:[{frame:"frame1",x:0,y:0,w:1277,h:704,sx:0.3, sy:0.3}],
+	currentFrameToShow:0,
+	};
+
+	entities["Bola1Img"] = {active:true, hidden:false,
+		position:{x:320.775,y:CANVAS_H/2}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame2",img:"../images/1.png"}],
+		framesToShow:[{frame:"frame2",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+		};
+	
+	entities["Bola2Img"] = {active:true, hidden:false,
+			position:{x:338.775,y:309.5}, w:40, h:40, rotation:0, flip:"none",
+			frames:[{index:"frame3",img:"../images/2.png"}],
+			framesToShow:[{frame:"frame3",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+			currentFrameToShow:0,
+		};
+	
+	entities["Bola3Img"] = {active:true, hidden:false,
+			position:{x:338.775,y:330.5}, w:40, h:40, rotation:0, flip:"none",
+			frames:[{index:"frame4",img:"../images/3.png"}],
+			framesToShow:[{frame:"frame4",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+			currentFrameToShow:0,
+	};
+
+	entities["Bola4Img"] = {active:true, hidden:false,
+		position:{x:356.775,y:299}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame5",img:"../images/4.png"}],
+		framesToShow:[{frame:"frame5",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola5Img"] = {active:true, hidden:false,
+		position:{x:356.775,y:CANVAS_H/2}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame6",img:"../images/5.png"}],
+		framesToShow:[{frame:"frame6",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola6Img"] = {active:true, hidden:false,
+		position:{x:356.775,y:341}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame7",img:"../images/6.png"}],
+		framesToShow:[{frame:"frame7",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+
+	entities["Bola7Img"] = {active:true, hidden:false,
+		position:{x:374.775,y:288.5}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame8",img:"../images/7.png"}],
+		framesToShow:[{frame:"frame8",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola8Img"] = {active:true, hidden:false,
+		position:{x:374.775,y:309.5}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame9",img:"../images/8.png"}],
+		framesToShow:[{frame:"frame9",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola9Img"] = {active:true, hidden:false,
+		position:{x:374.775,y:330.5}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame10",img:"../images/9.png"}],
+		framesToShow:[{frame:"frame10",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola10Img"] = {active:true, hidden:false,
+		position:{x:374.775,y:351.5}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame11",img:"../images/10.png"}],
+		framesToShow:[{frame:"frame11",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola11Img"] = {active:true, hidden:false,
+		position:{x:392.775,y:278}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame12",img:"../images/11.png"}],
+		framesToShow:[{frame:"frame12",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola12Img"] = {active:true, hidden:false,
+		position:{x:392.775,y:299}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame13",img:"../images/12.png"}],
+		framesToShow:[{frame:"frame13",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola13Img"] = {active:true, hidden:false,
+		position:{x:392.775,y:CANVAS_H/2}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame14",img:"../images/13.png"}],
+		framesToShow:[{frame:"frame14",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola14Img"] = {active:true, hidden:false,
+		position:{x:392.775,y:341}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame15",img:"../images/14.png"}],
+		framesToShow:[{frame:"frame15",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["Bola15Img"] = {active:true, hidden:false,
+		position:{x:392.775,y:362}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame16",img:"../images/15.png"}],
+		framesToShow:[{frame:"frame16",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["BolaBrancaImg"] = {active:true, hidden:false,
+		position:{x:CANVAS_W/2,y:CANVAS_H/2}, w:40, h:40, rotation:0, flip:"none",
+		frames:[{index:"frame17",img:"../images/cue_ball.png"}],
+		framesToShow:[{frame:"frame17",x:0,y:0,w:40,h:40,sx:0.5, sy:0.5}],
+		currentFrameToShow:0,
+	
+	};
+
+	entities["TacoImg"] = {active:true, hidden:false,
+		position:{x:CANVAS_W/2,y:CANVAS_H/2}, w:23, h:639, rotation:180, flip:"none",
+		frames:[{index:"frame18",img:"../images/Cue.png"}],
+		framesToShow:[{frame:"frame18",x:0,y:0,w:23,h:639,sx:0.5, sy:0.5, rotation:180}],
+		currentFrameToShow:0,
+	
+	};
+
+
+	asserts["moveSprite"] = {eval:"true", exec:"_moveToScreen('Bola1Img',system.mouse.x,system.mouse.y); system.play('Bola1Img')"};
+
+scenes["CenaPrincipal"] =
+	{title:"CenaPrincipal", active:true, h:CANVAS_H, w:CANVAS_W,
+		events:[
+			//{name:"mouseClicked", entity:"Bola1Img", assertToPerform:"moveSprite"}
+		],
+		components:[
+		{type:"sprite", entity:"TableImg"},
+		{type:"sprite", entity:"Bola1Img"},
+		{type:"sprite", entity:"Bola2Img"},
+		{type:"sprite", entity:"Bola3Img"},
+		{type:"sprite", entity:"Bola4Img"},
+		{type:"sprite", entity:"Bola5Img"},
+		{type:"sprite", entity:"Bola6Img"},
+		{type:"sprite", entity:"Bola7Img"},
+		{type:"sprite", entity:"Bola8Img"},
+		{type:"sprite", entity:"Bola9Img"},
+		{type:"sprite", entity:"Bola10Img"},
+		{type:"sprite", entity:"Bola11Img"},
+		{type:"sprite", entity:"Bola12Img"},
+		{type:"sprite", entity:"Bola13Img"},
+		{type:"sprite", entity:"Bola14Img"},
+		{type:"sprite", entity:"Bola15Img"},
+		{type:"sprite", entity:"BolaBrancaImg"},
+		{type:"sprite", entity:"TacoImg"}
+
+		]
+		
+	};
